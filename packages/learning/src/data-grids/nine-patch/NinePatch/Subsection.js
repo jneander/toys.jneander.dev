ad06295c5bf6bb90442b17c89815959c9ea3d4f8
Jeremy Neander <jneander@instructure.com>
2018-05-08 09:16:35 -0500

    feat(learning): add scroll syncing to 9-patch

--- packages/learning/src/data-grids/nine-patch/NinePatch/Subsection.js
+++ packages/learning/src/data-grids/nine-patch/NinePatch/Subsection.js
@@ -1,26 +1,68 @@
 import React, {PureComponent} from 'react'
 
+import ControlledView from './utils/ControlledView'
+import FixedText from './utils/FixedText'
 import styles from './styles.css'
 
 export default class Subsection extends PureComponent {
+  static defaultProps = {
+    horizontalScroll: false,
+    verticalScroll: false
+  }
+
+  constructor(props) {
+    super(props)
+
+    this._bindScrollParent = ref => {
+      this._scrollParent = ref
+    }
+
+    this._bindFixedText = ref => {
+      this._fixedText = ref
+    }
+
+    this._handleNodeScroll = () => {
+      const {scrollLeft, scrollTop} = this._scrollParent
+      this._fixedText.updateParentScrollPosition({left: scrollLeft, top: scrollTop})
+    }
+  }
+
+  componentDidMount() {
+    this._scrollParent.addEventListener('scroll', this._handleNodeScroll)
+  }
+
+  componentWillUnmount() {
+    this._scrollParent.removeEventListener('scroll', this._handleNodeScroll)
+  }
+
   render() {
     const contentStyle = {
       height: `${this.props.contentHeight}px`,
       width: `${this.props.contentWidth}px`
     }
 
-    const className = this.props.frozen
-      ? styles.FrozenSubsectionContainer
-      : styles.ScrollableSubsectionContainer
+    const className = this.props.verticalScroll
+      ? styles.ScrollableSubsectionContainer
+      : styles.FrozenSubsectionContainer
 
-    return (
-      <div className={className}>
+    let view = (
+      <div className={className} ref={this._bindScrollParent}>
         <div className={styles.Subsection} style={contentStyle}>
-          <span>
-            {this.props.contentWidth} x {this.props.contentHeight}
-          </span>
+          <FixedText ref={this._bindFixedText} />
         </div>
       </div>
     )
+
+    const {horizontalScroll, verticalScroll} = this.props
+
+    if (horizontalScroll || verticalScroll) {
+      view = (
+        <ControlledView horizontal={horizontalScroll} vertical={verticalScroll}>
+          {view}
+        </ControlledView>
+      )
+    }
+
+    return view
   }
 }
--- packages/learning/src/data-grids/nine-patch/NinePatch/index.js
+++ packages/learning/src/data-grids/nine-patch/NinePatch/index.js
@@ -1,29 +1,33 @@
 import React, {PureComponent} from 'react'
 
+import ViewController from './utils/ViewController'
 import Section from './Section'
 import styles from './styles.css'
 
 export default class NinePatch extends PureComponent {
   render() {
     return (
-      <div className={styles.Container}>
-        <Section
-          contentWidth={this.props.sectionWidths[0]}
-          frozen
-          sectionHeights={this.props.sectionHeights}
-        />
+      <ViewController>
+        {() => (
+          <div className={styles.Container}>
+            <Section
+              contentWidth={this.props.sectionWidths[0]}
+              sectionHeights={this.props.sectionHeights}
+            />
 
-        <Section
-          contentWidth={this.props.sectionWidths[1]}
-          sectionHeights={this.props.sectionHeights}
-        />
+            <Section
+              contentWidth={this.props.sectionWidths[1]}
+              horizontalScroll
+              sectionHeights={this.props.sectionHeights}
+            />
 
-        <Section
-          contentWidth={this.props.sectionWidths[2]}
-          frozen
-          sectionHeights={this.props.sectionHeights}
-        />
-      </div>
+            <Section
+              contentWidth={this.props.sectionWidths[2]}
+              sectionHeights={this.props.sectionHeights}
+            />
+          </div>
+        )}
+      </ViewController>
     )
   }
 }

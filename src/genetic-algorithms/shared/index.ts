9a870f0ae53378d3b67b91a8350ef92219ab11a9
Jeremy Neander <jeremy.neander@gmail.com>
2021-11-20 13:11:13 -0600

    refactor: rename base-controller

--- src/genetic-algorithms/shared/index.ts
+++ src/genetic-algorithms/shared/index.ts
@@ -1,7 +1,7 @@
 export {default as ChromosomeTable} from './ChromosomeTable'
-export {default as BaseController} from './Controller'
 export {default as ExampleControls} from './ExampleControls'
 export {default as Metrics} from './Metrics'
 
 export * from './chess-board'
+export * from './controller'
 export * from './types'

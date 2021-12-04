1725acb61ced2acc9602c164226abf17d7a11214
Jeremy Neander <jeremy.neander@gmail.com>
2021-12-04 10:50:04 -0600

    refactor: extract use-id function

---
+++ src/shared/components/utils/ids.tsx
@@ -0,0 +1,16 @@
+import {randomInt} from '@jneander/genetics'
+import {useMemo} from 'react'
+
+const idValues =
+  '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+export function randomId() {
+  return Array(8)
+    .fill(0)
+    .map(_ => idValues[randomInt(0, idValues.length)])
+    .join('')
+}
+
+export function useId(id?: string | number): string {
+  return useMemo(() => String(id ?? randomId()), [id])
+}

d7bc3627d599c38fad101e0847343879b4883926
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-08 11:52:06 -0600

    refactor: extract rectangle class

---
+++ src/simulations/carykh/evolution-simulator/Rectangle.ts
@@ -0,0 +1,13 @@
+export default class Rectangle {
+  x1: number
+  y1: number
+  x2: number
+  y2: number
+
+  constructor(tx1: number, ty1: number, tx2: number, ty2: number) {
+    this.x1 = tx1
+    this.y1 = ty1
+    this.x2 = tx2
+    this.y2 = ty2
+  }
+}

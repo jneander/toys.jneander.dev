6f2b4729bdb5beaa88053b6433f0a23726b5823c
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-10 17:33:28 -0600

    refactor: enumerate creature grid view type

--- src/simulations/carykh/evolution-simulator/constants.ts
+++ src/simulations/carykh/evolution-simulator/constants.ts
@@ -27,3 +27,10 @@ export enum Activity {
   PropagatingCreatures = 'PROPAGATING_CREATURES',
   PropagatedCreatures = 'PROPAGATED_CREATURES'
 }
+
+export enum CreatureGridViewType {
+  SimulationFinished = 'SIMULATION_FINISHED',
+  SortedCreatures = 'SORTED_CREATURES',
+  CulledCreatures = 'CULLED_CREATURES',
+  PropagatedCreatures = 'PROPAGATED_CREATURES'
+}

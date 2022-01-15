167daa6face9f023edcb5c33b98bf2bb324aaa27
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-15 16:02:20 -0600

    refactor: move app-view to app-view file

--- src/simulations/carykh/evolution-simulator/sketch.ts
+++ src/simulations/carykh/evolution-simulator/sketch.ts
@@ -1,5 +1,5 @@
 import type p5 from 'p5'
-import type {Color, Font, Graphics} from 'p5'
+import type {Font, Graphics} from 'p5'
 
 import Creature from './Creature'
 import Muscle from './Muscle'
@@ -29,6 +29,7 @@ import {
   NODE_OPERATION_LABELS_BY_ID
 } from './node-operations'
 import type {AppState, SimulationConfig, SimulationState} from './types'
+import {AppView} from './views'
 
 export default function sketch(p5: p5) {
   const AXON_COLOR = p5.color(255, 255, 0)
@@ -104,85 +105,6 @@ export default function sketch(p5: p5) {
     simulation,
     simulationState
   })
-
-  interface AppViewConfig {
-    font: Font
-    height: number
-    p5: p5
-    scale: number
-    width: number
-  }
-
-  class AppView {
-    height: number
-    scale: number
-    width: number
-
-    canvas: p5
-    screenGraphics: Graphics
-
-    constructor(config: AppViewConfig) {
-      this.height = config.height
-      this.scale = config.scale
-      this.width = config.width
-
-      this.canvas = config.p5
-      const {canvas} = this
-
-      // Create a 1024x576 Canvas
-      canvas.createCanvas(this.width * this.scale, this.height * this.scale)
-
-      this.screenGraphics = canvas.createGraphics(1920, 1080)
-
-      canvas.ellipseMode(canvas.CENTER)
-      canvas.textFont(config.font, 96)
-      canvas.textAlign(canvas.CENTER)
-
-      canvas.textFont(config.font, 96)
-      canvas.textAlign(canvas.CENTER)
-    }
-
-    getColor(i: number, adjust: boolean): Color {
-      const {canvas} = this
-
-      canvas.colorMode(canvas.HSB, 1.0)
-
-      let col = (i * 1.618034) % 1
-      if (i == 46) {
-        col = 0.083333
-      }
-
-      let light = 1.0
-      if (Math.abs(col - 0.333) <= 0.18 && adjust) {
-        light = 0.7
-      }
-
-      return canvas.color(col, 1.0, light)
-    }
-
-    getCursorPosition(): {cursorX: number; cursorY: number} {
-      const cursorX = this.canvas.mouseX / this.scale
-      const cursorY = this.canvas.mouseY / this.scale
-
-      return {cursorX, cursorY}
-    }
-
-    rectIsUnderCursor(
-      x: number,
-      y: number,
-      width: number,
-      height: number
-    ): boolean {
-      const {cursorX, cursorY} = this.getCursorPosition()
-
-      return (
-        cursorX >= x &&
-        cursorX <= x + width &&
-        cursorY >= y &&
-        cursorY <= y + height
-      )
-    }
-  }
 
   let appView: AppView
 

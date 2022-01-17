902bb48684bea9b96932dd7aaf8ba862a583c8c1
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-17 15:40:32 -0600

    refactor: move generate-creatures-activity to activities path

--- src/simulations/carykh/evolution-simulator/activities/index.ts
+++ src/simulations/carykh/evolution-simulator/activities/index.ts
@@ -1,3 +1,4 @@
+export * from './generate-creatures-activity'
 export * from './generation-view-activity'
 export * from './shared'
 export * from './start-activity'
--- src/simulations/carykh/evolution-simulator/sketch.ts
+++ src/simulations/carykh/evolution-simulator/sketch.ts
@@ -6,6 +6,7 @@ import Simulation from './Simulation'
 import {
   Activity,
   ActivityConfig,
+  GenerateCreaturesActivity,
   GenerationViewActivity,
   NullActivity,
   StartActivity
@@ -100,29 +101,6 @@ export default function sketch(p5: p5) {
   })
 
   let appView: AppView
-
-  class GeneratedCreaturesBackButton extends Widget {
-    draw(): void {
-      const {canvas, font, width} = this.appView
-
-      canvas.noStroke()
-      canvas.fill(100, 100, 200)
-      canvas.rect(900, 664, 260, 40)
-      canvas.fill(0)
-      canvas.textAlign(canvas.CENTER)
-      canvas.textFont(font, 24)
-      canvas.text('Back', width - 250, 690)
-    }
-
-    isUnderCursor(): boolean {
-      return this.appView.rectIsUnderCursor(900, 664, 260, 40)
-    }
-
-    onClick(): void {
-      this.appState.generationCount = 0
-      this.appController.setActivityId(ActivityId.GenerationView)
-    }
-  }
 
   class StepByStepSkipButton extends Widget {
     draw(): void {
@@ -326,80 +304,6 @@ export default function sketch(p5: p5) {
 
     onClick(): void {
       this.appController.setActivityId(ActivityId.GenerationView)
-    }
-  }
-
-  class GenerateCreaturesActivity extends Activity {
-    private creatureGridView: CreatureGridView
-    private backButton: GeneratedCreaturesBackButton
-
-    constructor(config: ActivityConfig) {
-      super(config)
-
-      const getCreatureAndGridIndexFn = (index: number) => {
-        return {
-          creature: this.appState.creaturesInLatestGeneration[index],
-          gridIndex: index
-        }
-      }
-
-      this.creatureGridView = new CreatureGridView({
-        appView: this.appView,
-        getCreatureAndGridIndexFn
-      })
-
-      this.backButton = new GeneratedCreaturesBackButton({
-        appController: this.appController,
-        appState: this.appState,
-        appView: this.appView
-      })
-    }
-
-    deinitialize(): void {
-      this.creatureGridView.deinitialize()
-    }
-
-    initialize(): void {
-      this.appController.generateCreatures()
-
-      this.drawInterface()
-      this.drawCreatureGrid()
-    }
-
-    onMouseReleased(): void {
-      if (this.backButton.isUnderCursor()) {
-        this.backButton.onClick()
-      }
-    }
-
-    private drawCreatureGrid(): void {
-      this.creatureGridView.draw()
-
-      const gridStartX = 25 // 40 minus horizontal grid margin
-      const gridStartY = 5 // 17 minus vertical grid margin
-
-      this.appView.canvas.image(
-        this.creatureGridView.graphics,
-        gridStartX,
-        gridStartY
-      )
-    }
-
-    private drawInterface(): void {
-      const {canvas, font, width} = this.appView
-
-      canvas.background(220, 253, 102)
-
-      canvas.noStroke()
-      canvas.fill(0)
-      canvas.textAlign(canvas.CENTER)
-      canvas.textFont(font, 24)
-      canvas.text(
-        `Here are your ${CREATURE_COUNT} randomly generated creatures!!!`,
-        width / 2 - 200,
-        690
-      )
-      this.backButton.draw()
     }
   }
 

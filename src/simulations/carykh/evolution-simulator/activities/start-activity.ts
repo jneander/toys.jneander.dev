8426a9239c18d5963bd6dc9ab9f9aaeddb847aaf
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-17 15:33:30 -0600

    refactor: move start-activity to activities path

---
+++ src/simulations/carykh/evolution-simulator/activities/start-activity.ts
@@ -0,0 +1,55 @@
+import {ActivityId} from '../constants'
+import {Widget} from '../views'
+import {Activity, ActivityConfig} from './shared'
+
+export class StartActivity extends Activity {
+  private startButton: StartViewStartButton
+
+  constructor(config: ActivityConfig) {
+    super(config)
+
+    this.startButton = new StartViewStartButton({
+      appController: this.appController,
+      appState: this.appState,
+      appView: this.appView
+    })
+  }
+
+  initialize(): void {
+    const {canvas, width} = this.appView
+
+    canvas.background(255)
+    canvas.noStroke()
+    canvas.fill(0)
+    canvas.text('EVOLUTION!', width / 2, 200)
+
+    this.startButton.draw()
+  }
+
+  onMouseReleased(): void {
+    if (this.startButton.isUnderCursor()) {
+      this.startButton.onClick()
+    }
+  }
+}
+
+class StartViewStartButton extends Widget {
+  draw(): void {
+    const {canvas, width} = this.appView
+
+    canvas.noStroke()
+    canvas.fill(100, 200, 100)
+    canvas.rect(width / 2 - 200, 300, 400, 200)
+    canvas.fill(0)
+    canvas.text('START', width / 2, 430)
+  }
+
+  isUnderCursor(): boolean {
+    const {appView} = this
+    return appView.rectIsUnderCursor(appView.width / 2 - 200, 300, 400, 200)
+  }
+
+  onClick(): void {
+    this.appController.setActivityId(ActivityId.GenerationView)
+  }
+}

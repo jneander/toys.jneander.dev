546b18205b33239258b0df96eebc379d51844590
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-17 15:20:16 -0600

    refactor: inject simulation state and config into dependents

--- src/simulations/carykh/evolution-simulator/activities/shared.ts
+++ src/simulations/carykh/evolution-simulator/activities/shared.ts
@@ -1,11 +1,13 @@
 import type {AppController} from '../app-controller'
-import type {AppState} from '../types'
+import type {AppState, SimulationConfig, SimulationState} from '../types'
 import type {AppView} from '../views'
 
 export interface ActivityConfig {
   appController: AppController
   appState: AppState
   appView: AppView
+  simulationConfig: SimulationConfig
+  simulationState: SimulationState
 }
 
 export interface ActivityInterface {
@@ -21,11 +23,15 @@ export abstract class Activity implements ActivityInterface {
   protected appController: AppController
   protected appState: AppState
   protected appView: AppView
+  protected simulationConfig: SimulationConfig
+  protected simulationState: SimulationState
 
   constructor(config: ActivityConfig) {
     this.appController = config.appController
     this.appState = config.appState
     this.appView = config.appView
+    this.simulationConfig = config.simulationConfig
+    this.simulationState = config.simulationState
   }
 
   initialize(): void {}

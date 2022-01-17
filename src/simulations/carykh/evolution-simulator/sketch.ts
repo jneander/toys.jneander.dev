c6dfdf1628389cbe3b9bec0ff650f155ccb2652d
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-17 15:52:08 -0600

    refactor: move sorted-creatures-activity to activities path

--- src/simulations/carykh/evolution-simulator/sketch.ts
+++ src/simulations/carykh/evolution-simulator/sketch.ts
@@ -11,6 +11,7 @@ import {
   NullActivity,
   SimulationFinishedActivity,
   SimulationRunningActivity,
+  SortedCreaturesActivity,
   SortingCreaturesActivity,
   StartActivity
 } from './activities'
@@ -93,32 +94,6 @@ export default function sketch(p5: p5) {
 
   let appView: AppView
 
-  class CullCreaturesButton extends Widget {
-    draw(): void {
-      const {canvas, font, screenGraphics, width} = this.appView
-
-      screenGraphics.noStroke()
-      screenGraphics.fill(100, 100, 200)
-      screenGraphics.rect(900, 670, 260, 40)
-      screenGraphics.fill(0)
-      screenGraphics.textAlign(canvas.CENTER)
-      screenGraphics.textFont(font, 24)
-      screenGraphics.text(
-        `Kill ${Math.floor(CREATURE_COUNT / 2)}`,
-        width - 250,
-        700
-      )
-    }
-
-    isUnderCursor(): boolean {
-      return this.appView.rectIsUnderCursor(900, 670, 260, 40)
-    }
-
-    onClick(): void {
-      this.appController.setActivityId(ActivityId.CullCreatures)
-    }
-  }
-
   class PropagateCreaturesButton extends Widget {
     draw(): void {
       const {canvas, font, screenGraphics, width} = this.appView
@@ -160,110 +135,6 @@ export default function sketch(p5: p5) {
 
     onClick(): void {
       this.appController.setActivityId(ActivityId.GenerationView)
-    }
-  }
-
-  class SortedCreaturesActivity extends Activity {
-    private creatureGridView: CreatureGridView
-    private popupSimulationView: PopupSimulationView
-    private cullCreaturesButton: CullCreaturesButton
-
-    constructor(config: ActivityConfig) {
-      super(config)
-
-      const getCreatureAndGridIndexFn = (index: number) => {
-        return {
-          creature: this.appState.sortedCreatures[index],
-          gridIndex: index
-        }
-      }
-
-      this.creatureGridView = new CreatureGridView({
-        appView: this.appView,
-        getCreatureAndGridIndexFn
-      })
-
-      const widgetConfig = {
-        appController: this.appController,
-        appState: this.appState,
-        appView: this.appView
-      }
-
-      const simulationWidgetConfig = {
-        ...widgetConfig,
-        simulationConfig: this.simulationConfig,
-        simulationState: this.simulationState
-      }
-
-      this.popupSimulationView = new PopupSimulationView(simulationWidgetConfig)
-      this.cullCreaturesButton = new CullCreaturesButton(widgetConfig)
-    }
-
-    deinitialize(): void {
-      this.creatureGridView.deinitialize()
-      this.popupSimulationView.deinitialize()
-    }
-
-    draw(): void {
-      const {canvas, height, screenGraphics, width} = this.appView
-      const {creatureGridView} = this
-
-      canvas.image(screenGraphics, 0, 0, width, height)
-
-      const gridStartX = 25 // 40 minus horizontal grid margin
-      const gridStartY = 28 // 40 minus vertical grid margin
-
-      canvas.image(creatureGridView.graphics, gridStartX, gridStartY)
-
-      /*
-       * When the cursor is over any of the creature tiles, the popup simulation
-       * will be displayed for the associated creature.
-       */
-
-      const gridIndex = creatureGridView.getGridIndexUnderCursor(40, 42)
-
-      if (gridIndex != null) {
-        this.appController.setPopupSimulationCreatureId(gridIndex)
-        this.popupSimulationView.draw()
-      } else {
-        this.appController.clearPopupSimulation()
-      }
-    }
-
-    initialize(): void {
-      this.drawInterface()
-      this.creatureGridView.draw()
-    }
-
-    onMouseReleased(): void {
-      if (this.cullCreaturesButton.isUnderCursor()) {
-        this.cullCreaturesButton.onClick()
-      }
-    }
-
-    private drawInterface(): void {
-      const {canvas, font, screenGraphics, width} = this.appView
-
-      screenGraphics.background(220, 253, 102)
-
-      screenGraphics.push()
-      screenGraphics.scale(1.5)
-
-      screenGraphics.textAlign(canvas.CENTER)
-      screenGraphics.textFont(font, 24)
-      screenGraphics.fill(100, 100, 200)
-      screenGraphics.noStroke()
-
-      screenGraphics.fill(0)
-      screenGraphics.text('Fastest creatures at the top!', width / 2, 30)
-      screenGraphics.text(
-        'Slowest creatures at the bottom. (Going backward = slow)',
-        width / 2 - 200,
-        700
-      )
-      this.cullCreaturesButton.draw()
-
-      screenGraphics.pop()
     }
   }
 

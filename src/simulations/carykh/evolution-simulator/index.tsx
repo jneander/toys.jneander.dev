910eaea969135d0a2fb3ed0f531f988a0b776054
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-23 15:15:27 -0600

    refactor: inject app-controller and app-state into sketch function

--- src/simulations/carykh/evolution-simulator/index.tsx
+++ src/simulations/carykh/evolution-simulator/index.tsx
@@ -1,6 +1,12 @@
+import {AleaNumberGenerator} from '@jneander/utils-random'
 import {ReactNode, useEffect, useState} from 'react'
 
-import sketch from './sketch'
+import {NullActivity} from './activities'
+import {AppController} from './app-controller'
+import {ActivityId} from './constants'
+import {SimulationConfig} from './simulation'
+import {createSketchFn} from './sketch'
+import type {AppState} from './types'
 
 export function CarykhEvolutionSimulator() {
   const [content, setContent] = useState<ReactNode>(null)
@@ -10,8 +16,36 @@ export function CarykhEvolutionSimulator() {
 
     import('../../../shared/p5').then(module => {
       if (mounted) {
+        const SEED = 0
+        const randomNumberGenerator = new AleaNumberGenerator({seed: SEED})
+
+        const appState: AppState = {
+          creaturesInLatestGeneration: [],
+          currentActivity: new NullActivity(),
+          currentActivityId: null,
+          generationCount: -1,
+          generationHistoryMap: {},
+          nextActivityId: ActivityId.Start,
+          selectedGeneration: 0
+        }
+
+        const simulationConfig: SimulationConfig = {
+          hazelStairs: -1
+        }
+
+        const appController = new AppController({
+          appState,
+          randomNumberGenerator,
+          simulationConfig
+        })
+
+        const sketchFn = createSketchFn({
+          appController,
+          appState
+        })
+
         const {P5View} = module
-        setContent(<P5View sketch={sketch}></P5View>)
+        setContent(<P5View sketch={sketchFn}></P5View>)
       }
     })
 
--- src/simulations/carykh/evolution-simulator/sketch.ts
+++ src/simulations/carykh/evolution-simulator/sketch.ts
@@ -1,4 +1,3 @@
-import {AleaNumberGenerator} from '@jneander/utils-random'
 import type p5 from 'p5'
 import type {Font} from 'p5'
 
@@ -6,7 +5,6 @@ import {
   CullCreaturesActivity,
   GenerateCreaturesActivity,
   GenerationViewActivity,
-  NullActivity,
   PropagateCreaturesActivity,
   SimulationFinishedActivity,
   SimulationRunningActivity,
@@ -14,101 +12,86 @@ import {
   SortingCreaturesActivity,
   StartActivity
 } from './activities'
-import {AppController} from './app-controller'
+import type {AppController} from './app-controller'
 import {ActivityId} from './constants'
-import type {SimulationConfig} from './simulation'
 import type {AppState} from './types'
 import {AppView} from './views'
 
-export default function sketch(p5: p5) {
-  const FRAME_RATE = 60 // target frames per second
-  const SEED = 0
+export interface CreateSketchFnConfig {
+  appController: AppController
+  appState: AppState
+}
 
-  const randomNumberGenerator = new AleaNumberGenerator({seed: SEED})
+export function createSketchFn({
+  appController,
+  appState
+}: CreateSketchFnConfig) {
+  return function sketch(p5: p5) {
+    const FRAME_RATE = 60 // target frames per second
 
-  let font: Font
+    let appView: AppView
+    let font: Font
 
-  const appState: AppState = {
-    creaturesInLatestGeneration: [],
-    currentActivity: new NullActivity(),
-    currentActivityId: null,
-    generationCount: -1,
-    generationHistoryMap: {},
-    nextActivityId: ActivityId.Start,
-    selectedGeneration: 0
-  }
-
-  const simulationConfig: SimulationConfig = {
-    hazelStairs: -1
-  }
-
-  const appController = new AppController({
-    appState,
-    randomNumberGenerator,
-    simulationConfig
-  })
-
-  let appView: AppView
-
-  const activityClassByActivityId = {
-    [ActivityId.Start]: StartActivity,
-    [ActivityId.GenerationView]: GenerationViewActivity,
-    [ActivityId.GenerateCreatures]: GenerateCreaturesActivity,
-    [ActivityId.SimulationRunning]: SimulationRunningActivity,
-    [ActivityId.SimulationFinished]: SimulationFinishedActivity,
-    [ActivityId.SortingCreatures]: SortingCreaturesActivity,
-    [ActivityId.SortedCreatures]: SortedCreaturesActivity,
-    [ActivityId.CullCreatures]: CullCreaturesActivity,
-    [ActivityId.PropagateCreatures]: PropagateCreaturesActivity
-  }
-
-  p5.mouseWheel = (event: WheelEvent) => {
-    appState.currentActivity.onMouseWheel(event)
-  }
-
-  p5.mousePressed = () => {
-    appState.currentActivity.onMousePressed()
-  }
-
-  p5.mouseReleased = () => {
-    appState.currentActivity.onMouseReleased()
-  }
-
-  p5.preload = () => {
-    font = p5.loadFont('/fonts/Helvetica-Bold.otf')
-  }
-
-  p5.setup = () => {
-    p5.frameRate(FRAME_RATE)
-
-    appView = new AppView({
-      font,
-      height: 720,
-      p5,
-      scale: 0.8,
-      width: 1280
-    })
-  }
-
-  p5.draw = () => {
-    p5.scale(appView.scale)
-
-    const {currentActivityId, nextActivityId} = appState
-
-    if (nextActivityId !== currentActivityId) {
-      appState.currentActivity.deinitialize()
-
-      const ActivityClass = activityClassByActivityId[nextActivityId]
-      appState.currentActivity = new ActivityClass({
-        appController,
-        appState,
-        appView
-      })
-      appState.currentActivityId = nextActivityId
-
-      appState.currentActivity.initialize()
+    const activityClassByActivityId = {
+      [ActivityId.Start]: StartActivity,
+      [ActivityId.GenerationView]: GenerationViewActivity,
+      [ActivityId.GenerateCreatures]: GenerateCreaturesActivity,
+      [ActivityId.SimulationRunning]: SimulationRunningActivity,
+      [ActivityId.SimulationFinished]: SimulationFinishedActivity,
+      [ActivityId.SortingCreatures]: SortingCreaturesActivity,
+      [ActivityId.SortedCreatures]: SortedCreaturesActivity,
+      [ActivityId.CullCreatures]: CullCreaturesActivity,
+      [ActivityId.PropagateCreatures]: PropagateCreaturesActivity
     }
 
-    appState.currentActivity.draw()
+    p5.mouseWheel = (event: WheelEvent) => {
+      appState.currentActivity.onMouseWheel(event)
+    }
+
+    p5.mousePressed = () => {
+      appState.currentActivity.onMousePressed()
+    }
+
+    p5.mouseReleased = () => {
+      appState.currentActivity.onMouseReleased()
+    }
+
+    p5.preload = () => {
+      font = p5.loadFont('/fonts/Helvetica-Bold.otf')
+    }
+
+    p5.setup = () => {
+      p5.frameRate(FRAME_RATE)
+
+      appView = new AppView({
+        font,
+        height: 720,
+        p5,
+        scale: 0.8,
+        width: 1280
+      })
+    }
+
+    p5.draw = () => {
+      p5.scale(appView.scale)
+
+      const {currentActivityId, nextActivityId} = appState
+
+      if (nextActivityId !== currentActivityId) {
+        appState.currentActivity.deinitialize()
+
+        const ActivityClass = activityClassByActivityId[nextActivityId]
+        appState.currentActivity = new ActivityClass({
+          appController,
+          appState,
+          appView
+        })
+        appState.currentActivityId = nextActivityId
+
+        appState.currentActivity.initialize()
+      }
+
+      appState.currentActivity.draw()
+    }
   }
 }

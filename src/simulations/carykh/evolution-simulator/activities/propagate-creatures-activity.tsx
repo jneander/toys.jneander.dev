0a909fdb1899130b22246f02c7a282f1fb5b4234
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-25 09:56:17 -0600

    refactor: partially replace propagate-creatures-activity canvas with html

--- src/simulations/carykh/evolution-simulator/activities/propagate-creatures-activity.tsx
+++ src/simulations/carykh/evolution-simulator/activities/propagate-creatures-activity.tsx
@@ -1,12 +1,11 @@
-import type {Graphics} from 'p5'
 import {useMemo} from 'react'
 
 import {P5ClientView} from '../../../../shared/p5'
 import type {AppController} from '../app-controller'
-import {ActivityId} from '../constants'
+import {ActivityId, CREATURE_COUNT} from '../constants'
 import {createSketchFn} from '../sketch'
 import type {AppStore} from '../types'
-import {ButtonWidget, ButtonWidgetConfig, CreatureGridView} from '../views'
+import {CreatureGridView} from '../views'
 import {Activity, ActivityConfig} from './shared'
 
 export interface PropagateCreaturesActivityProps {
@@ -23,14 +22,32 @@ export function PropagateCreaturesActivity(
     return createSketchFn({appController, appStore})
   }, [appController, appStore])
 
-  return <P5ClientView sketch={sketchFn} />
+  function handleBackClick() {
+    appController.setActivityId(ActivityId.GenerationView)
+  }
+
+  return (
+    <div>
+      <div style={{height: '576px'}}>
+        <P5ClientView sketch={sketchFn} />
+      </div>
+
+      <p>
+        These are the {CREATURE_COUNT} creatures of generation #
+        {appStore.getState().generationCount + 1}.
+      </p>
+
+      <p>What perils will they face? Find out next time!</p>
+
+      <button onClick={handleBackClick} type="button">
+        Back
+      </button>
+    </div>
+  )
 }
 
 export class PropagateCreaturesP5Activity extends Activity {
   private creatureGridView: CreatureGridView
-  private backButton: BackButton
-
-  private graphics: Graphics
 
   constructor(config: ActivityConfig) {
     super(config)
@@ -42,36 +59,19 @@ export class PropagateCreaturesP5Activity extends Activity {
       }
     }
 
-    this.graphics = this.appView.canvas.createGraphics(1920, 1080)
-
     this.creatureGridView = new CreatureGridView({
       appView: this.appView,
       getCreatureAndGridIndexFn,
       gridStartX: 40,
       gridStartY: 40
     })
-
-    this.backButton = new BackButton({
-      appView: this.appView,
-      graphics: this.graphics,
-
-      onClick: () => {
-        this.appController.setActivityId(ActivityId.GenerationView)
-      }
-    })
   }
 
   initialize(): void {
     this.appController.propagateCreatures()
 
-    this.drawInterface()
+    this.appView.canvas.background(220, 253, 102)
     this.initializeCreatureGrid()
-  }
-
-  onMouseReleased(): void {
-    if (this.backButton.isUnderCursor()) {
-      this.backButton.onClick()
-    }
   }
 
   private initializeCreatureGrid(): void {
@@ -83,69 +83,5 @@ export class PropagateCreaturesP5Activity extends Activity {
     const gridStartY = 28 // 40 minus vertical grid margin
 
     canvas.image(this.creatureGridView.graphics, gridStartX, gridStartY)
-  }
-
-  private drawInterface(): void {
-    const {appStore, appView, graphics} = this
-    const {canvas, font, height, width} = appView
-
-    graphics.background(220, 253, 102)
-
-    graphics.push()
-    graphics.scale(1.5)
-
-    graphics.textAlign(canvas.CENTER)
-    graphics.textFont(font, 24)
-    graphics.fill(100, 100, 200)
-    graphics.noStroke()
-
-    graphics.fill(0)
-    graphics.text(
-      'These are the 1000 creatures of generation #' +
-        (appStore.getState().generationCount + 1) +
-        '.',
-      width / 2,
-      30
-    )
-    graphics.text(
-      'What perils will they face?  Find out next time!',
-      width / 2 - 130,
-      700
-    )
-    this.backButton.draw()
-
-    graphics.pop()
-
-    canvas.image(graphics, 0, 0, width, height)
-  }
-}
-
-interface SkipButtonConfig extends ButtonWidgetConfig {
-  graphics: Graphics
-}
-
-class BackButton extends ButtonWidget {
-  private graphics: Graphics
-
-  constructor(config: SkipButtonConfig) {
-    super(config)
-
-    this.graphics = config.graphics
-  }
-  draw(): void {
-    const {canvas, font, width} = this.appView
-    const {graphics} = this
-
-    graphics.noStroke()
-    graphics.fill(100, 100, 200)
-    graphics.rect(1050, 670, 160, 40)
-    graphics.fill(0)
-    graphics.textAlign(canvas.CENTER)
-    graphics.textFont(font, 24)
-    graphics.text('Back', width - 150, 700)
-  }
-
-  isUnderCursor(): boolean {
-    return this.appView.rectIsUnderCursor(1050, 670, 160, 40)
   }
 }

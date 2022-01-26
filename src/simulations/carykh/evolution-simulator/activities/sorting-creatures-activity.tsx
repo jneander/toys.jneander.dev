da42b8f121af0db2203da655ad5ada19c5ee92f4
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-26 11:59:49 -0600

    refactor: clarify math in sorting-creatures-activity

--- src/simulations/carykh/evolution-simulator/activities/sorting-creatures-activity.tsx
+++ src/simulations/carykh/evolution-simulator/activities/sorting-creatures-activity.tsx
@@ -7,7 +7,11 @@ import {CreatureDrawer} from '../creature-drawer'
 import {creatureIdToIndex} from '../helpers'
 import {CreateActivityFnParameters, createSketchFn} from '../sketch'
 import type {AppStore} from '../types'
-import {CREATURE_GRID_TILES_PER_ROW} from '../views'
+import {
+  CREATURE_GRID_TILES_PER_ROW,
+  CREATURE_GRID_TILE_HEIGHT,
+  CREATURE_GRID_TILE_WIDTH
+} from '../views'
 import {Activity, ActivityConfig} from './shared'
 
 export interface SortingCreaturesActivityProps {
@@ -65,12 +69,28 @@ class SortingCreaturesP5Activity extends Activity {
     const {appStore, appView} = this
     const {canvas} = appView
 
+    const scale = 10
+
     canvas.background(220, 253, 102)
     canvas.push()
-    canvas.scale(10.0 / SCALE_TO_FIX_BUG)
+    canvas.scale(scale / SCALE_TO_FIX_BUG)
+
+    const creatureScale = 0.1
 
     const transition =
       0.5 - 0.5 * Math.cos(Math.min(this.activityTimer / 60, Math.PI))
+
+    const gridStartX = 40 * creatureScale
+    /*
+     * The next grid is lower on canvas, so interpolate between the vertical
+     * positions of the previous grid and the next.
+     */
+    const gridStartY =
+      this.interpolate(15, 15 + CREATURE_GRID_TILE_HEIGHT, transition) *
+      creatureScale
+
+    const scaledCreatureWidth = CREATURE_GRID_TILE_WIDTH * creatureScale
+    const scaledCreatureHeight = CREATURE_GRID_TILE_HEIGHT * creatureScale
 
     for (let i2 = 0; i2 < CREATURE_COUNT; i2++) {
       // i2 is the index of where the creature is now
@@ -82,14 +102,17 @@ class SortingCreaturesP5Activity extends Activity {
       const x1 = i1 % CREATURE_GRID_TILES_PER_ROW
       const y1 = Math.floor(i1 / CREATURE_GRID_TILES_PER_ROW)
       const x2 = i2 % CREATURE_GRID_TILES_PER_ROW
-      const y2 = Math.floor(i2 / CREATURE_GRID_TILES_PER_ROW) + 1 // next grid is lower on canvas
+      const y2 = Math.floor(i2 / CREATURE_GRID_TILES_PER_ROW)
       const x3 = this.interpolate(x1, x2, transition)
       const y3 = this.interpolate(y1, y2, transition)
 
+      const creatureCenterX = x3 * scaledCreatureWidth + scaledCreatureWidth / 2
+      const creatureBottomY = y3 * scaledCreatureHeight + scaledCreatureHeight
+
       this.creatureDrawer.drawCreature(
         creature,
-        x3 * 3 + 5.5,
-        y3 * 2.5 + 4,
+        creatureCenterX + gridStartX,
+        creatureBottomY + gridStartY,
         canvas
       )
     }

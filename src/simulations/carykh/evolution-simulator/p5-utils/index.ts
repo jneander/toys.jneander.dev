12c7b1d1779e47c70d9b17b5a8a4f0a9e64fc84b
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-26 16:10:58 -0600

    refactor: move get-species-color function to p5-utils helpers

--- src/simulations/carykh/evolution-simulator/p5-utils/index.ts
+++ src/simulations/carykh/evolution-simulator/p5-utils/index.ts
@@ -1,24 +1,2 @@
+export * from './helpers'
 export * from './p5-wrapper'
-
-import type p5 from 'p5'
-import type {Color} from 'p5'
-
-export function getSpeciesColor(
-  p5: p5,
-  speciesId: number,
-  adjust: boolean
-): Color {
-  p5.colorMode(p5.HSB, 1.0)
-
-  let col = (speciesId * 1.618034) % 1
-  if (speciesId == 46) {
-    col = 0.083333
-  }
-
-  let light = 1.0
-  if (Math.abs(col - 0.333) <= 0.18 && adjust) {
-    light = 0.7
-  }
-
-  return p5.color(col, 1.0, light)
-}

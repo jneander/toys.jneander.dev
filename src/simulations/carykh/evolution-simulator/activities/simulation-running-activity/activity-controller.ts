5c2215a6f53bce038331c60119f9209c94488ee4
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-27 08:31:28 -0600

    refactor: create generation-simulation in activity-controller

--- src/simulations/carykh/evolution-simulator/activities/simulation-running-activity/activity-controller.ts
+++ src/simulations/carykh/evolution-simulator/activities/simulation-running-activity/activity-controller.ts
@@ -1,12 +1,35 @@
+import type {AppController} from '../../app-controller'
+import {GenerationSimulation} from '../../simulation'
+import type {AppStore} from '../../types'
 import type {ActivityState} from './types'
+
+export interface ActivityControllerConfig {
+  appController: AppController
+  appStore: AppStore
+}
 
 export class ActivityController {
   private state: ActivityState
 
-  constructor() {
+  private generationSimulation: GenerationSimulation
+
+  constructor(config: ActivityControllerConfig) {
+    const {appController, appStore} = config
+
+    this.generationSimulation = new GenerationSimulation({
+      appStore,
+      simulationConfig: appController.getSimulationConfig()
+    })
+
+    this.generationSimulation.initialize()
+
     this.state = {
       timer: 0
     }
+  }
+
+  getGenerationSimulation(): GenerationSimulation {
+    return this.generationSimulation
   }
 
   getTimer(): number {

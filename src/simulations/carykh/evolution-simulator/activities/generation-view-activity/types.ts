bb242b0879007b05dec89e7e28757aeae2a96076
Jeremy Neander <jeremy.neander@gmail.com>
2022-01-28 08:40:31 -0600

    refactor: move some generation-view-activity state into activity-store

---
+++ src/simulations/carykh/evolution-simulator/activities/generation-view-activity/types.ts
@@ -0,0 +1,10 @@
+import type {Store} from '@jneander/utils-state'
+
+import type {GenerationSimulationMode} from '../../constants'
+
+export type ActivityState = {
+  generationSimulationMode: GenerationSimulationMode
+  pendingGenerationCount: number
+}
+
+export type ActivityStore = Store<ActivityState>

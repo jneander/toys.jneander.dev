b191990299ce2387138f6f658074da291204b770
Jeremy Neander <jeremy.neander@gmail.com>
2022-02-02 07:50:46 -0600

    refactor: use controlled loop for generation simulations

--- src/simulations/carykh/evolution-simulator/activities/generation-view-activity/types.ts
+++ src/simulations/carykh/evolution-simulator/activities/generation-view-activity/types.ts
@@ -1,8 +1,10 @@
 import type {Store} from '@jneander/utils-state'
 
+import type {GenerationSimulation} from '../../simulation'
 import type {GenerationSimulationMode} from './constants'
 
 export type ActivityState = {
+  currentGenerationSimulation: GenerationSimulation | null
   generationSimulationMode: GenerationSimulationMode
   pendingGenerationCount: number
 }

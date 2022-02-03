52e23a10f84bf080bc1e577149bc4d9d65778a79
Jeremy Neander <jeremy.neander@gmail.com>
2022-02-03 09:47:52 -0600

    refactor: move creature-grid sketch function into view-controller

--- src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/simulation-finished-view/view-controller.ts
+++ src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/simulation-finished-view/view-controller.ts
@@ -1,16 +1,19 @@
 import type p5 from 'p5'
+import type {Font} from 'p5'
 
 import type {AppController} from '../../../app-controller'
+import {P5Wrapper} from '../../../p5-utils'
 import type {AppStore} from '../../../types'
 import {CreatureGridP5UI} from './creature-grid-p5-ui'
 import type {CreatureGridViewConfig} from './p5-view'
-import {createSketchFn, CreateUiFnParameters} from './sketch'
 
 export interface ViewControllerConfig {
   appController: AppController
   appStore: AppStore
   getCreatureAndGridIndexFn: CreatureGridViewConfig['getCreatureAndGridIndexFn']
 }
+
+let font: Font
 
 export class ViewController {
   private container: HTMLElement | null
@@ -35,8 +38,48 @@ export class ViewController {
     const p5 = (await import('p5')).default
 
     if (this.container != null) {
-      const createUiFn = ({p5Wrapper}: CreateUiFnParameters) => {
-        return new CreatureGridP5UI({
+      this.p5Instance = new p5(this.sketch.bind(this), this.container)
+    }
+  }
+
+  deinitialize() {
+    this.p5Instance?.remove()
+    this.container = null
+  }
+
+  private sketch(p5: p5): void {
+    const FRAME_RATE = 60 // target frames per second
+
+    let currentUI: CreatureGridP5UI
+    let p5Wrapper: P5Wrapper
+
+    p5.mouseReleased = () => {
+      currentUI.onMouseReleased()
+    }
+
+    if (font == null) {
+      p5.preload = () => {
+        font = p5.loadFont('/fonts/Helvetica-Bold.otf')
+      }
+    }
+
+    p5.setup = () => {
+      p5.frameRate(FRAME_RATE)
+
+      p5Wrapper = new P5Wrapper({
+        font,
+        height: 720,
+        p5,
+        scale: 0.8,
+        width: 1280
+      })
+    }
+
+    p5.draw = () => {
+      p5.scale(p5Wrapper.scale)
+
+      if (currentUI == null) {
+        currentUI = new CreatureGridP5UI({
           appController: this.appController,
           appStore: this.appStore,
           getCreatureAndGridIndexFn: this.getCreatureAndGridIndexFn,
@@ -45,15 +88,11 @@ export class ViewController {
           p5Wrapper,
           showsPopupSimulation: () => true
         })
+
+        currentUI.initialize()
       }
 
-      const sketchFn = createSketchFn({createUiFn})
-      this.p5Instance = new p5(sketchFn, this.container)
+      currentUI.draw()
     }
-  }
-
-  deinitialize() {
-    this.p5Instance?.remove()
-    this.container = null
   }
 }

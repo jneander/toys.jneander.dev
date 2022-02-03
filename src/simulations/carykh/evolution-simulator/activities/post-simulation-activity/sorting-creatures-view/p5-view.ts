0e6d78f9a466d9ed71bd794ba3ea6638c44b09f9
Jeremy Neander <jeremy.neander@gmail.com>
2022-02-02 18:35:22 -0600

    refactor: improve sorting-creatures-view draw performance

--- src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/sorting-creatures-view/p5-view.ts
+++ src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/sorting-creatures-view/p5-view.ts
@@ -1,3 +1,5 @@
+import type {Graphics, Image} from 'p5'
+
 import {CREATURE_COUNT, SCALE_TO_FIX_BUG} from '../../../constants'
 import {CreatureDrawer} from '../../../creature-drawer'
 import {
@@ -5,7 +7,7 @@ import {
   CREATURE_GRID_TILE_HEIGHT,
   CREATURE_GRID_TILE_WIDTH
 } from '../../../creature-grid'
-import {creatureIdToIndex} from '../../../creatures'
+import {Creature, creatureIdToIndex} from '../../../creatures'
 import {P5Wrapper} from '../../../p5-utils'
 import type {AppStore} from '../../../types'
 import type {ActivityController} from '../activity-controller'
@@ -17,11 +19,18 @@ export interface SortingCreaturesP5ViewConfig {
   p5Wrapper: P5Wrapper
 }
 
+type CreatureImageCache = {
+  [creatureId: number]: Image
+}
+
 export class SortingCreaturesP5View {
   private activityController: ActivityController
   private appStore: AppStore
   private p5Wrapper: P5Wrapper
   private creatureDrawer: CreatureDrawer
+
+  private creatureGraphics: Graphics
+  private creatureImageCache: CreatureImageCache
 
   private activityTimer: number
 
@@ -31,6 +40,12 @@ export class SortingCreaturesP5View {
     this.p5Wrapper = config.p5Wrapper
 
     this.creatureDrawer = new CreatureDrawer({p5Wrapper: this.p5Wrapper})
+
+    this.creatureGraphics = this.p5Wrapper.canvas.createGraphics(
+      CREATURE_GRID_TILE_WIDTH * 3,
+      CREATURE_GRID_TILE_HEIGHT * 3
+    )
+    this.creatureImageCache = {}
 
     this.activityTimer = 0
   }
@@ -73,23 +88,62 @@ export class SortingCreaturesP5View {
       const creatureCenterX = x3 * scaledCreatureWidth + scaledCreatureWidth / 2
       const creatureBottomY = y3 * scaledCreatureHeight + scaledCreatureHeight
 
-      this.creatureDrawer.drawCreature(
-        creature,
-        creatureCenterX + gridStartX,
-        creatureBottomY + gridStartY,
-        canvas
+      const creatureImage = this.getCreatureImage(creature)
+      canvas.image(
+        creatureImage,
+        (creatureCenterX + gridStartX - scaledCreatureWidth * 1.5) *
+          SCALE_TO_FIX_BUG,
+        (creatureBottomY + gridStartY - scaledCreatureHeight * 2) *
+          SCALE_TO_FIX_BUG,
+        scaledCreatureWidth * 3 * SCALE_TO_FIX_BUG,
+        scaledCreatureHeight * 3 * SCALE_TO_FIX_BUG
       )
     }
 
     canvas.pop()
 
-    this.activityTimer += 10
+    this.activityTimer += 1
 
     if (this.activityTimer > 60 * Math.PI) {
       this.activityController.setCurrentActivityStep(
         ActivityStep.SortedCreatures
       )
     }
+  }
+
+  private getCreatureImage(creature: Creature): Image {
+    let image = this.creatureImageCache[creature.id]
+
+    if (image != null) {
+      return image
+    }
+
+    this.creatureGraphics.clear()
+
+    this.creatureGraphics.push()
+
+    // Translate to the bottom center of where the creature is drawn.
+    this.creatureGraphics.translate(
+      this.creatureGraphics.width / 2,
+      (this.creatureGraphics.height * 2) / 3
+    )
+    // Scale to fit the creature in the center.
+    this.creatureGraphics.scale(10 / SCALE_TO_FIX_BUG)
+
+    this.creatureDrawer.drawCreature(creature, 0, 0, this.creatureGraphics)
+
+    this.creatureGraphics.pop()
+
+    image = this.creatureGraphics.get(
+      0,
+      0,
+      this.creatureGraphics.width,
+      this.creatureGraphics.height
+    )
+
+    this.creatureImageCache[creature.id] = image
+
+    return image
   }
 
   private interpolate(a: number, b: number, offset: number): number {

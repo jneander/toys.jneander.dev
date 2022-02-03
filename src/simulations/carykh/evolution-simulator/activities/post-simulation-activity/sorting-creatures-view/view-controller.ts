21c4fd504a89b3f9d7ab1dbb60316d8c6ec8f2d1
Jeremy Neander <jeremy.neander@gmail.com>
2022-02-03 07:06:46 -0600

    refactor: move sorting-creatures-view sketch function into view-controller

--- src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/sorting-creatures-view/view-controller.ts
+++ src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/sorting-creatures-view/view-controller.ts
@@ -1,14 +1,17 @@
 import type p5 from 'p5'
+import type {Font} from 'p5'
 
+import {P5Wrapper} from '../../../p5-utils'
 import type {AppStore} from '../../../types'
 import type {ActivityController} from '../activity-controller'
 import {SortingCreaturesP5View} from './p5-view'
-import {createSketchFn, CreateUiFnParameters} from './sketch'
 
 export interface ViewControllerConfig {
   activityController: ActivityController
   appStore: AppStore
 }
+
+let font: Font
 
 export class ViewController {
   private container: HTMLElement | null
@@ -31,21 +34,51 @@ export class ViewController {
     const p5 = (await import('p5')).default
 
     if (this.container != null) {
-      const createUiFn = ({p5Wrapper}: CreateUiFnParameters) => {
-        return new SortingCreaturesP5View({
-          activityController: this.activityController,
-          appStore: this.appStore,
-          p5Wrapper
-        })
-      }
-
-      const sketchFn = createSketchFn({createUiFn})
-      this.p5Instance = new p5(sketchFn, this.container)
+      this.p5Instance = new p5(this.sketch.bind(this), this.container)
     }
   }
 
   deinitialize() {
     this.p5Instance?.remove()
     this.container = null
+  }
+
+  private sketch(p5: p5): void {
+    const FRAME_RATE = 60 // target frames per second
+
+    let currentUI: SortingCreaturesP5View
+    let p5Wrapper: P5Wrapper
+
+    if (font == null) {
+      p5.preload = () => {
+        font = p5.loadFont('/fonts/Helvetica-Bold.otf')
+      }
+    }
+
+    p5.setup = () => {
+      p5.frameRate(FRAME_RATE)
+
+      p5Wrapper = new P5Wrapper({
+        font,
+        height: 720,
+        p5,
+        scale: 0.8,
+        width: 1280
+      })
+    }
+
+    p5.draw = () => {
+      p5.scale(p5Wrapper.scale)
+
+      if (currentUI == null) {
+        currentUI = new SortingCreaturesP5View({
+          activityController: this.activityController,
+          appStore: this.appStore,
+          p5Wrapper
+        })
+      }
+
+      currentUI.draw()
+    }
   }
 }

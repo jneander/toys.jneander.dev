9a3bb0e42c0f7e429ce6612ef633660e5c64a871
Jeremy Neander <jeremy.neander@gmail.com>
2022-02-04 11:40:48 -0600

    refactor: remove direct activity references from creature-grid-adapter

--- src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/simulation-finished-view/creature-grid-adapter.ts
+++ src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/simulation-finished-view/creature-grid-adapter.ts
@@ -1,44 +1,36 @@
 import type {AppController} from '../../../app-controller'
 import type {P5ClientViewAdapter, P5Wrapper} from '../../../p5-utils'
 import type {AppStore} from '../../../types'
-import type {ActivityController} from '../activity-controller'
+import type {CreatureAndGridIndex} from '../types'
 import {CreatureGridP5UI} from './creature-grid-p5-ui'
 
 export interface CreatureGridAdapterConfig {
-  activityController: ActivityController
   appController: AppController
   appStore: AppStore
+  getCreatureAndGridIndexFn: (index: number) => CreatureAndGridIndex
+  showsPopupSimulation: () => boolean
 }
 
 export class CreatureGridAdapter implements P5ClientViewAdapter {
-  private activityController: ActivityController
-  private appController: AppController
-  private appStore: AppStore
+  private config: CreatureGridAdapterConfig
 
   private creatureGridP5Ui: CreatureGridP5UI | null
 
   constructor(config: CreatureGridAdapterConfig) {
-    this.activityController = config.activityController
-    this.appController = config.appController
-    this.appStore = config.appStore
+    this.config = config
 
     this.creatureGridP5Ui = null
   }
 
   initialize(p5Wrapper: P5Wrapper): void {
-    const getCreatureAndGridIndexFn = (index: number) => {
-      return this.activityController.getCreatureAndGridIndex(index)
-    }
-
     this.creatureGridP5Ui = new CreatureGridP5UI({
-      appController: this.appController,
-      appStore: this.appStore,
-      getCreatureAndGridIndexFn,
+      appController: this.config.appController,
+      appStore: this.config.appStore,
+      getCreatureAndGridIndexFn: this.config.getCreatureAndGridIndexFn,
       gridStartX: 40,
       gridStartY: 42,
       p5Wrapper,
-      showsPopupSimulation: () =>
-        this.activityController.currentStepShowsPopupSimulation()
+      showsPopupSimulation: this.config.showsPopupSimulation
     })
 
     this.creatureGridP5Ui.initialize()

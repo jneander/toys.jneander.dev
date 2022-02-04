6992dc3ab1081b1111f53bfeaa2733c201ace21a
Jeremy Neander <jeremy.neander@gmail.com>
2022-02-04 11:33:02 -0600

    refactor: remove direct activity references from sorting-creatures-adapter

--- src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/sorting-creatures-view/sorting-creatures-adapter.ts
+++ src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/sorting-creatures-view/sorting-creatures-adapter.ts
@@ -1,37 +1,27 @@
 import type {P5ClientViewAdapter, P5Wrapper} from '../../../p5-utils'
 import type {AppStore} from '../../../types'
-import type {ActivityController} from '../activity-controller'
-import {ActivityStep} from '../constants'
 import {SortingCreaturesP5View} from './sorting-creatures-p5-view'
 
 export interface SortingCreaturesAdapterConfig {
-  activityController: ActivityController
   appStore: AppStore
+  onAnimationFinished: () => void
 }
 
 export class SortingCreaturesAdapter implements P5ClientViewAdapter {
-  private activityController: ActivityController
-  private appStore: AppStore
+  private config: SortingCreaturesAdapterConfig
 
   private sortingCreaturesP5Ui: SortingCreaturesP5View | null
 
   constructor(config: SortingCreaturesAdapterConfig) {
-    this.activityController = config.activityController
-    this.appStore = config.appStore
+    this.config = config
 
     this.sortingCreaturesP5Ui = null
   }
 
   initialize(p5Wrapper: P5Wrapper): void {
-    const onAnimationFinished = () => {
-      this.activityController.setCurrentActivityStep(
-        ActivityStep.SortedCreatures
-      )
-    }
-
     this.sortingCreaturesP5Ui = new SortingCreaturesP5View({
-      appStore: this.appStore,
-      onAnimationFinished,
+      appStore: this.config.appStore,
+      onAnimationFinished: this.config.onAnimationFinished,
       p5Wrapper
     })
   }

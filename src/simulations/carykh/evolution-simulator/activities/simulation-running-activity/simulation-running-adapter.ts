42b26fe2f7be01fb57a5438a98015e6b50cfe0cd
Jeremy Neander <jeremy.neander@gmail.com>
2022-02-04 16:12:10 -0600

    refactor: use p5-controlled-client-view in simulation-running-activity

---
+++ src/simulations/carykh/evolution-simulator/activities/simulation-running-activity/simulation-running-adapter.ts
@@ -0,0 +1,43 @@
+import type {AppController} from '../../app-controller'
+import type {P5ClientViewAdapter, P5Wrapper} from '../../p5-utils'
+import type {ActivityController} from './activity-controller'
+import {SimulationRunningP5Ui} from './simulation-running-p5-ui'
+
+export interface SimulationRunningAdapterConfig {
+  activityController: ActivityController
+  appController: AppController
+}
+
+export class SimulationRunningAdapter implements P5ClientViewAdapter {
+  private config: SimulationRunningAdapterConfig
+
+  private simulationRunningP5Ui: SimulationRunningP5Ui | null
+
+  constructor(config: SimulationRunningAdapterConfig) {
+    this.config = config
+
+    this.simulationRunningP5Ui = null
+  }
+
+  initialize(p5Wrapper: P5Wrapper): void {
+    this.simulationRunningP5Ui = new SimulationRunningP5Ui({
+      activityController: this.config.activityController,
+      appController: this.config.appController,
+      p5Wrapper
+    })
+
+    this.simulationRunningP5Ui.initialize()
+  }
+
+  deinitialize(): void {
+    this.simulationRunningP5Ui = null
+  }
+
+  draw(): void {
+    this.simulationRunningP5Ui?.draw()
+  }
+
+  onMouseWheel(event: WheelEvent): void {
+    this.simulationRunningP5Ui?.onMouseWheel(event)
+  }
+}

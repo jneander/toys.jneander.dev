d570924bcc5fafdcf017bcd5311006aa92227156
Jeremy Neander <jeremy.neander@gmail.com>
2023-04-08 19:26:32 -0500

    refactor: rename entry files

--- config/routes.js
+++ config/routes.js
@@ -1,36 +1,37 @@
 module.exports.routes = [
   {
     key: 'card-splitting',
-    component: 'src/pages/genetic-algorithms/show-card-splitting/index.tsx',
+    component: 'src/pages/genetic-algorithms/show-card-splitting/card-splitting.entry.tsx',
   },
 
   {
     key: 'knight-covering',
-    component: 'src/pages/genetic-algorithms/show-knight-covering/index.tsx',
+    component: 'src/pages/genetic-algorithms/show-knight-covering/knight-covering.entry.tsx',
   },
 
   {
     key: 'one-max',
-    component: 'src/pages/genetic-algorithms/show-one-max/index.tsx',
+    component: 'src/pages/genetic-algorithms/show-one-max/one-max.entry.tsx',
   },
 
   {
     key: 'queens',
-    component: 'src/pages/genetic-algorithms/show-queens/index.tsx',
+    component: 'src/pages/genetic-algorithms/show-queens/queens.entry.tsx',
   },
 
   {
     key: 'sorting-numbers',
-    component: 'src/pages/genetic-algorithms/show-sorting-numbers/index.tsx',
+    component: 'src/pages/genetic-algorithms/show-sorting-numbers/sorting-numbers.entry.tsx',
   },
 
   {
     key: 'text-matching',
-    component: 'src/pages/genetic-algorithms/show-text-matching/index.tsx',
+    component: 'src/pages/genetic-algorithms/show-text-matching/text-matching.entry.tsx',
   },
 
   {
     key: 'carykh-evolution-simulator',
-    component: 'src/pages/simulations/carykh/show-evolution-simulator/index.tsx',
+    component:
+      'src/pages/simulations/carykh/show-evolution-simulator/carykh-evolution-simulator.entry.tsx',
   },
 ]

a2d0a52ac2b589eeecc9cb2202ae2c775837ba14
Jeremy Neander <jeremy.neander@gmail.com>
2023-04-13 08:44:14 -0500

    refactor: use event-bus to control propagation start

--- src/genetic-algorithms/one-max/one-max.tsx
+++ src/genetic-algorithms/one-max/one-max.tsx
@@ -28,7 +28,6 @@ export function OneMax() {
         maxPropagationSpeed={state.maxPropagationSpeed}
         onPause={controller.stop}
         onRefresh={controller.randomizeTarget}
-        onStart={controller.start}
         playing={state.isRunning}
         propagationSpeed={state.propagationSpeed}
         rangePosition={state.playbackPosition}

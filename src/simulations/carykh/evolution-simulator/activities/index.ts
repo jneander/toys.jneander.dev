f979760e9845283b7c2d1933e087d07500b769a9
Jeremy Neander <jeremy.neander@gmail.com>
2023-04-16 17:31:10 -0500

    refactor: convert simulation-running-activity to lit

--- src/simulations/carykh/evolution-simulator/activities/index.ts
+++ src/simulations/carykh/evolution-simulator/activities/index.ts
@@ -1,2 +1,1 @@
 export * from './post-simulation-activity'
-export * from './simulation-running-activity'
--- src/simulations/carykh/evolution-simulator/activities/simulation-running-activity/element.ts
+++ src/simulations/carykh/evolution-simulator/activities/simulation-running-activity/element.ts
@@ -1,14 +1,25 @@
-import {ComponentProps, createElement} from 'react'
+import '../../p5-utils/p5-controlled-client-view.element'
 
-import {defineElement} from '../../../../../shared/views'
+import {Store} from '@jneander/utils-state'
+import {html} from 'lit'
+
+import {BaseElement, defineElement} from '../../../../../shared/views'
 import type {AppController} from '../../app-controller'
+import {SIMULATION_SPEED_INITIAL} from '../../constants'
 import type {AppStore} from '../../types'
-import {ReactLitElement} from '../../views'
-import {SimulationRunningActivity} from '.'
+import {ActivityController} from './activity-controller'
+import {SimulationRunningAdapter} from './simulation-running-adapter'
+import type {ActivityState} from './types'
 
-export class SimulationRunningActivityElement extends ReactLitElement {
+import styles from './styles.module.scss'
+
+export class SimulationRunningActivityElement extends BaseElement {
   public declare controller: AppController
   public declare store: AppStore
+
+  private activityController?: ActivityController
+  private activityStore?: Store<ActivityState>
+  private clientViewAdapter?: SimulationRunningAdapter
 
   static get properties() {
     return {
@@ -17,13 +28,57 @@ export class SimulationRunningActivityElement extends ReactLitElement {
     }
   }
 
-  protected createElement() {
-    const props: ComponentProps<typeof SimulationRunningActivity> = {
+  connectedCallback(): void {
+    this.activityStore = new Store<ActivityState>({
+      simulationSpeed: SIMULATION_SPEED_INITIAL,
+      timer: 0,
+    })
+
+    this.activityController = new ActivityController({
+      activityStore: this.activityStore,
       appController: this.controller,
       appStore: this.store,
-    }
+    })
 
-    return createElement(SimulationRunningActivity, props)
+    this.clientViewAdapter = new SimulationRunningAdapter({
+      activityController: this.activityController,
+      appController: this.controller,
+    })
+
+    super.connectedCallback()
+  }
+
+  protected render(): unknown {
+    const {simulationSpeed} = this.activityStore?.getState() ?? {}
+
+    return html`
+      <div>
+        <div class=${styles.Container}>
+          <p5-controlled-client-view .clientViewAdapter=${this.clientViewAdapter} />
+        </div>
+
+        <button @click=${this.handleSkipClick} type="button">Skip</button>
+
+        <button @click=${this.handlePlaybackSpeedClick} type="button">
+          Playback Speed: x${simulationSpeed}
+        </button>
+
+        <button @click=${this.handleFinishClick} type="button">Finish</button>
+      </div>
+    `
+  }
+
+  private handleSkipClick(): void {
+    this.activityController?.advanceGenerationSimulation()
+  }
+
+  private handlePlaybackSpeedClick(): void {
+    this.activityController?.increaseSimulationSpeed()
+    this.requestUpdate()
+  }
+
+  private handleFinishClick(): void {
+    this.activityController?.finishGenerationSimulation()
   }
 }
 

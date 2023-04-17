e5edaac079c54462bf490495c3ec87048548b167
Jeremy Neander <jeremy.neander@gmail.com>
2023-04-17 07:58:03 -0500

    refactor: convert post-simulation-activity to lit

--- src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/element.ts
+++ src/simulations/carykh/evolution-simulator/activities/post-simulation-activity/element.ts
@@ -1,14 +1,32 @@
-import {ComponentProps, createElement} from 'react'
+import {Store} from '@jneander/utils-state'
+import {html} from 'lit'
+import {createElement} from 'react'
+import {createRoot, Root} from 'react-dom/client'
 
-import {defineElement} from '../../../../../shared/views'
+import {BaseElement, defineElement} from '../../../../../shared/views'
 import type {AppController} from '../../app-controller'
+import {CreatureGridAdapter, SortingCreaturesAdapter} from '../../creature-collection-view'
+import {P5ClientViewAdapter} from '../../p5-utils'
 import type {AppStore} from '../../types'
-import {ReactLitElement} from '../../views'
-import {PostSimulationActivity} from '.'
+import {ActivityController} from './activity-controller'
+import {ActivityStep} from './constants'
+import {CullCreaturesView} from './cull-creatures-view'
+import {PropagateCreaturesView} from './propagate-creatures-view'
+import {SimulationFinishedView} from './simulation-finished-view'
+import {SortedCreaturesView} from './sorted-creatures-view'
+import {SortingCreaturesView} from './sorting-creatures-view'
+import type {ActivityState} from './types'
 
-export class PostSimulationActivityElement extends ReactLitElement {
+export class PostSimulationActivityElement extends BaseElement {
   public declare controller: AppController
   public declare store: AppStore
+
+  private activityViewRoot?: Root
+  private activityStore?: Store<ActivityState>
+  private activityController?: ActivityController
+  private creatureCollectionAdapter?: P5ClientViewAdapter
+
+  private storeListeners: (() => void)[] = []
 
   static get properties() {
     return {
@@ -17,13 +35,135 @@ export class PostSimulationActivityElement extends ReactLitElement {
     }
   }
 
-  protected createElement() {
-    const props: ComponentProps<typeof PostSimulationActivity> = {
+  connectedCallback(): void {
+    this.activityStore = new Store<ActivityState>({
+      currentActivityStep: ActivityStep.SimulationFinished,
+    })
+
+    this.activityController = new ActivityController({
+      activityStore: this.activityStore,
       appController: this.controller,
       appStore: this.store,
+    })
+
+    this.storeListeners.push(
+      this.activityStore.subscribe(() => {
+        this.requestUpdate()
+      }),
+    )
+
+    super.connectedCallback()
+  }
+
+  disconnectedCallback(): void {
+    this.storeListeners.forEach(fn => {
+      fn()
+    })
+    this.storeListeners.length = 0
+
+    this.activityViewRoot?.unmount()
+
+    super.disconnectedCallback()
+  }
+
+  protected update(changedProperties: Map<PropertyKey, unknown>): void {
+    const {currentActivityStep} = this.activityStore?.getState() ?? {}
+
+    if (currentActivityStep === ActivityStep.SortingCreatures) {
+      const onAnimationFinished = () => {
+        this.activityController?.setCurrentActivityStep(ActivityStep.SortedCreatures)
+      }
+
+      this.creatureCollectionAdapter = new SortingCreaturesAdapter({
+        appStore: this.store,
+        onAnimationFinished,
+      })
+    } else {
+      const getCreatureAndGridIndexFn = (index: number) => {
+        if (this.activityController == null) {
+          throw new Error('ActivityController is not defined')
+        }
+
+        return this.activityController.getCreatureAndGridIndex(index) || 0
+      }
+
+      const showsPopupSimulation = () =>
+        this.activityController?.currentStepShowsPopupSimulation() || false
+
+      this.creatureCollectionAdapter = new CreatureGridAdapter({
+        appController: this.controller,
+        appStore: this.store,
+        getCreatureAndGridIndexFn,
+        showsPopupSimulation,
+      })
     }
 
-    return createElement(PostSimulationActivity, props)
+    super.update(changedProperties)
+  }
+
+  protected firstUpdated(): void {
+    const activityViewContainer = this.querySelector('#activity-view')
+    if (activityViewContainer) {
+      this.activityViewRoot = createRoot(activityViewContainer)
+    }
+  }
+
+  protected updated(): void {
+    const {currentActivityStep} = this.activityStore?.getState() ?? {}
+
+    if (!(this.activityController && this.controller && this.store)) {
+      return
+    }
+
+    let element
+
+    if (currentActivityStep === ActivityStep.SortingCreatures) {
+      element = createElement(SortingCreaturesView, {
+        activityController: this.activityController,
+      })
+    }
+
+    if (currentActivityStep === ActivityStep.SortedCreatures) {
+      element = createElement(SortedCreaturesView, {
+        activityController: this.activityController,
+        appController: this.controller,
+      })
+    }
+
+    if (currentActivityStep === ActivityStep.CullCreatures) {
+      element = createElement(CullCreaturesView, {
+        activityController: this.activityController,
+        appController: this.controller,
+      })
+    }
+
+    if (currentActivityStep === ActivityStep.PropagateCreatures) {
+      element = createElement(PropagateCreaturesView, {
+        activityController: this.activityController,
+        appStore: this.store,
+      })
+    }
+
+    if (currentActivityStep === ActivityStep.SimulationFinished) {
+      element = createElement(SimulationFinishedView, {
+        activityController: this.activityController,
+        appController: this.controller,
+      })
+    }
+
+    this.activityViewRoot?.render(element)
+  }
+
+  protected render() {
+    return html`
+      <div class="flow">
+        <creature-collection-view
+          .adapter=${this.creatureCollectionAdapter}
+        ></creature-collection-view>
+
+        <div id="activity-view"></div>
+      </div>
+    `
   }
 }
 
--- src/simulations/carykh/evolution-simulator/p5-utils/index.ts
+++ src/simulations/carykh/evolution-simulator/p5-utils/index.ts
@@ -1,4 +1,3 @@
 export * from './p5-client-view-controller'
-export * from './p5-controlled-client-view'
 export * from './p5-wrapper'
 export * from './types'

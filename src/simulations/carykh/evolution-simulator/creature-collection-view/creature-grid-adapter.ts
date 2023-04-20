72d21bfd5ea3e7e05a245cb858b286e118dc08b6
Jeremy Neander <jeremy.neander@gmail.com>
2023-04-20 09:19:27 -0500

    refactor: merge ui views into adapters

--- src/simulations/carykh/evolution-simulator/creature-collection-view/creature-grid-adapter.ts
+++ src/simulations/carykh/evolution-simulator/creature-collection-view/creature-grid-adapter.ts
@@ -1,7 +1,16 @@
 import type {AppController} from '../app-controller'
 import type {P5ViewAdapter, P5ViewDimensions, P5Wrapper} from '../p5-utils'
 import type {AppStore} from '../types'
-import {CreatureGridP5Ui} from './creature-grid-p5-ui'
+import {
+  CREATURE_GRID_TILE_HEIGHT,
+  CREATURE_GRID_TILE_WIDTH,
+  VIEW_PADDING_END_X,
+  VIEW_PADDING_START_X,
+  VIEW_PADDING_START_Y,
+} from './constants'
+import {CreatureGridP5View} from './creature-grid-p5-view'
+import {gridIndexToRowAndColumn} from './helpers'
+import {PopupSimulationView, PopupSimulationViewAnchor} from './popup-simulation-view'
 import type {CreatureAndGridIndex} from './types'
 
 export interface CreatureGridAdapterConfig {
@@ -14,40 +23,81 @@ export interface CreatureGridAdapterConfig {
 export class CreatureGridAdapter implements P5ViewAdapter {
   private config: CreatureGridAdapterConfig
 
-  private creatureGridP5Ui: CreatureGridP5Ui | null
+  private creatureGridView?: CreatureGridP5View
+  private p5Wrapper?: P5Wrapper
+  private popupSimulationView?: PopupSimulationView
 
   constructor(config: CreatureGridAdapterConfig) {
     this.config = config
-
-    this.creatureGridP5Ui = null
   }
 
   initialize(p5Wrapper: P5Wrapper): void {
+    this.p5Wrapper = p5Wrapper
+
     const {height, width} = this.dimensions
     p5Wrapper.updateCanvasSize(width, height)
 
-    this.creatureGridP5Ui = new CreatureGridP5Ui({
-      appController: this.config.appController,
-      appStore: this.config.appStore,
+    this.creatureGridView = new CreatureGridP5View({
       dimensions: this.dimensions,
       getCreatureAndGridIndexFn: this.config.getCreatureAndGridIndexFn,
       p5Wrapper,
-      showsPopupSimulation: this.config.showsPopupSimulation,
+      showsHoverState: this.config.showsPopupSimulation,
     })
 
-    this.creatureGridP5Ui.initialize()
+    this.popupSimulationView = new PopupSimulationView({
+      p5Wrapper,
+      simulationConfig: this.config.appController.getSimulationConfig(),
+    })
+
+    this.creatureGridView.initialize()
   }
 
   deinitialize(): void {
-    this.creatureGridP5Ui = null
+    delete this.creatureGridView
+    delete this.popupSimulationView
+    delete this.p5Wrapper
   }
 
   draw(): void {
-    this.creatureGridP5Ui?.draw()
+    const {creatureGridView, p5Wrapper, popupSimulationView} = this
+
+    if (!(creatureGridView && p5Wrapper && popupSimulationView)) {
+      return
+    }
+
+    const {p5} = p5Wrapper
+
+    p5.clear(0, 0, 0, 0)
+
+    creatureGridView.draw()
+    p5.image(creatureGridView.graphics, 0, 0)
+
+    if (!this.config.showsPopupSimulation()) {
+      return
+    }
+
+    /*
+     * When the cursor is over any of the creature tiles, the popup simulation
+     * will be displayed for the associated creature.
+     */
+
+    const gridIndex = creatureGridView.getGridIndexUnderCursor()
+
+    if (gridIndex != null) {
+      this.setPopupSimulationCreatureInfo(gridIndex)
+
+      const anchor = this.calculateAnchorForPopupSimulation(gridIndex)
+      popupSimulationView.setAnchor(anchor)
+
+      popupSimulationView.draw()
+    } else {
+      this.clearPopupSimulation()
+    }
   }
 
   onMouseReleased() {
-    this.creatureGridP5Ui?.onMouseReleased()
+    // When the popup simulation is running, mouse clicks will stop it.
+    this.popupSimulationView?.dismissSimulationView()
   }
 
   private get dimensions(): P5ViewDimensions {
@@ -55,5 +105,42 @@ export class CreatureGridAdapter implements P5ViewAdapter {
       height: 664,
       width: 1024,
     }
+  }
+
+  private setPopupSimulationCreatureInfo(generationCreatureIndex: number): void {
+    const creature =
+      this.config.appStore.getState().creaturesInLatestGeneration[generationCreatureIndex]
+
+    this.popupSimulationView?.setCreatureInfo({
+      creature,
+      rank: generationCreatureIndex + 1,
+    })
+  }
+
+  private clearPopupSimulation(): void {
+    this.popupSimulationView?.setCreatureInfo(null)
+  }
+
+  private calculateAnchorForPopupSimulation(gridIndex: number): PopupSimulationViewAnchor {
+    const {columnIndex, rowIndex} = gridIndexToRowAndColumn(
+      gridIndex,
+      this.getMaxCreatureTilesPerRow(),
+    )
+
+    const tileStartX = VIEW_PADDING_START_X + columnIndex * CREATURE_GRID_TILE_WIDTH
+    const tileStartY = VIEW_PADDING_START_Y + rowIndex * CREATURE_GRID_TILE_HEIGHT
+
+    return {
+      startPositionX: tileStartX,
+      startPositionY: tileStartY,
+      endPositionX: tileStartX + CREATURE_GRID_TILE_WIDTH,
+      endPositionY: tileStartY + CREATURE_GRID_TILE_HEIGHT,
+      margin: 5,
+    }
+  }
+
+  private getMaxCreatureTilesPerRow(): number {
+    const gridAreaWidth = this.dimensions.width - VIEW_PADDING_START_X - VIEW_PADDING_END_X
+    return Math.floor(gridAreaWidth / CREATURE_GRID_TILE_WIDTH)
   }
 }

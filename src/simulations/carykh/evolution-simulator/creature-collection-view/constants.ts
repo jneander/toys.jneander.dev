6c3ba55f7bf507c54ca3e72f6660896f4e4bbe73
Jeremy Neander <jeremy.neander@gmail.com>
2023-04-21 10:30:41 -0500

    feat: decrease creature grid padding

--- src/simulations/carykh/evolution-simulator/creature-collection-view/constants.ts
+++ src/simulations/carykh/evolution-simulator/creature-collection-view/constants.ts
@@ -1,7 +1,7 @@
 export const CREATURE_GRID_TILE_WIDTH = 24
 export const CREATURE_GRID_TILE_HEIGHT = 24
 
-export const VIEW_PADDING_START_X = 32
-export const VIEW_PADDING_START_Y = 32
-export const VIEW_PADDING_END_X = 32
-export const VIEW_PADDING_END_Y = 32
+export const VIEW_PADDING_START_X = 12
+export const VIEW_PADDING_START_Y = 12
+export const VIEW_PADDING_END_X = 12
+export const VIEW_PADDING_END_Y = 12

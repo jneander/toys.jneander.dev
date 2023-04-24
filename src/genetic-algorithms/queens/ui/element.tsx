c68312984255cff5884ed8aad995ca51e806990c
Jeremy Neander <jeremy.neander@gmail.com>
2023-04-24 09:01:10 -0500

    feat: convert queens to custom elements

--- src/genetic-algorithms/queens/ui/element.tsx
+++ src/genetic-algorithms/queens/ui/element.tsx
@@ -1,45 +1,92 @@
-import {EventBus} from '@jneander/event-bus'
-import {createRoot, Root} from 'react-dom/client'
+import '../../shared/chess-board/chess-board-element'
+import '../../shared/example-controls/element'
+import './configuration-element'
 
+import {EventBus} from '@jneander/event-bus'
+import {html} from 'lit'
+
+import {BaseElement} from '../../../shared/views'
 import {createControlsStore} from '../../shared'
 import {Controller} from '../controller'
 import {createStore} from '../state'
-import {Queens} from './component'
 
-export class QueensElement extends HTMLElement {
+import styles from '../styles.module.scss'
+
+export class QueensElement extends BaseElement {
   private controller?: Controller
-  private root?: Root
+  private controlsStore?: ReturnType<typeof createControlsStore>
+  private eventBus?: EventBus
+  private store?: ReturnType<typeof createStore>
+  private storeListeners: (() => void)[] = []
 
   connectedCallback() {
-    const controlsStore = createControlsStore()
-    const eventBus = new EventBus()
-    const store = createStore()
+    this.controlsStore = createControlsStore()
+    this.eventBus = new EventBus()
+    this.store = createStore()
 
     this.controller = new Controller({
-      controlsStore,
-      eventBus,
-      store,
+      controlsStore: this.controlsStore,
+      eventBus: this.eventBus,
+      store: this.store,
     })
 
-    const container = document.createElement('div')
-    container.classList.add('flow')
-    this.appendChild(container)
+    this.storeListeners.push(
+      this.controlsStore.subscribe(() => {
+        this.requestUpdate()
+      }),
 
-    this.root = createRoot(container)
-    this.root.render(
-      <Queens
-        controller={this.controller}
-        controlsStore={controlsStore}
-        eventBus={eventBus}
-        store={store}
-      />,
+      this.store.subscribe(() => {
+        this.requestUpdate()
+      }),
     )
 
     this.controller.initialize()
+
+    super.connectedCallback()
   }
 
   disconnectedCallback() {
+    this.storeListeners.forEach(fn => {
+      fn()
+    })
+    this.storeListeners.length = 0
+
     this.controller?.deinitialize()
-    this.root?.unmount()
+
+    super.disconnectedCallback()
+  }
+
+  protected render() {
+    if (this.store == null || this.controlsStore == null) {
+      return
+    }
+
+    const {boardSize, current} = this.store.getState()
+    const {isRunning} = this.controlsStore.getState()
+
+    const handleBoardSizeChange = (size: number) => {
+      this.controller?.setBoardSize(size)
+    }
+
+    return html`
+      <div class="${styles.Container} flow">
+        <queens-configuration
+          boardSize=${boardSize}
+          ?disabled=${isRunning}
+          .onBoardSizeChange=${handleBoardSizeChange}
+        ></queens-configuration>
+
+        <div>
+          <chess-board .positions=${current?.chromosome?.genes} size=${boardSize}></chess-board>
+        </div>
+
+        <div>Iteration: ${current?.iteration ?? 0}</div>
+
+        <example-controls
+          .eventBus=${this.eventBus}
+          .store=${this.controlsStore}
+        ></example-controls>
+      </div>
+    `
   }
 }
--- src/genetic-algorithms/shared/chess-board/constants.ts
+++ src/genetic-algorithms/shared/chess-board/constants.ts
@@ -1,3 +1,2 @@
 export const EMPTY_PIECE = '	'
 export const KNIGHT_UNICODE = '&#9822;'
-export const QUEEN_UNICODE = '&#9819;'
